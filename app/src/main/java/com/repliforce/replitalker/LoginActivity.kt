b9c187a6a27ae27722ec80d28515af6f2a0785a6
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-08-26 22:28:34 -0300

    feature: Rename LoginActivity.

--- app/src/main/java/com/repliforce/replitalker/LoginActivity.kt
+++ app/src/main/java/com/repliforce/replitalker/LoginActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.repliforce.replitalker.ui.theme.RepliTalkerTheme
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
