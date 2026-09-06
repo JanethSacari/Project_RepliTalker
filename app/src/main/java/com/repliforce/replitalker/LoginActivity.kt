5680ffff806ba0e118819f53198690742f6909fb
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2026-09-06 18:46:21 -0300

    feature: Implemented user email and password authenticator.

--- app/src/main/java/com/repliforce/replitalker/LoginActivity.kt
+++ app/src/main/java/com/repliforce/replitalker/LoginActivity.kt
@@ -2,14 +2,58 @@ package com.repliforce.replitalker
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.repliforce.replitalker.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState:Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)}
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser?.uid
+        user?.let {}
+    }
+    private lateinit var binding: ActivityLoginBinding
 
-    fun onLogin(v: View) {}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onLogin(v: View) {
+        var proceed = true
+        if (binding.emailET.text.isNullOrEmpty()) {
+            binding.emailTIL.error = "Email is required"
+            binding.emailTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (binding.passwordET.text.isNullOrEmpty()) {
+            binding.passwordTIL.error = "Password is required"
+            binding.passwordTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (proceed) {
+            binding.loginProgressLayout.visibility = View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(
+                binding.emailET.text.toString(),
+                binding.passwordET.text.toString()
+            ).addOnCompleteListener() {
+                task ->
+                if (!task.isSuccessful) {
+                    binding.loginProgressLayout.visibility = View.GONE
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login Error: ${task.exception?.localizedMessage}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+                binding.loginProgressLayout.visibility = View.GONE
+            }
+        }
+    }
     fun goToSignup(v: View) {}
 }
